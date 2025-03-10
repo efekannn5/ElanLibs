36bde8a727c3f2ab6116599c1a136e97c82a7806
Efekan Nefesoglu <efekan8190nefesogeu@gmail.com>
2025-03-10 13:54:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elan",  # PyPI'de görünecek paket adı
-    version="0.1.0",
+    version="0.1.3",
     packages=find_packages(include=["src", "src.*"]),
     install_requires=[],  # Bağımlılıklar buraya eklenebilir (örn: numpy, pandas)
     author="Efekan Nefesoğlu",

4dbf2b17cced7e67e2510d5010d94180b71d823b
Efekan Nefesoglu <efekan8190nefesogeu@gmail.com>
2025-03-12 00:55:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="elan",  # PyPI'de görünecek paket adı
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=['cv2'],  # Bağımlılıklar buraya eklenebilir (örn: numpy, pandas)
